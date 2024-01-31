9cef90e4e5252738981e608b52ebfd46beea288f
Siva Priyan <sprverse@gmail.com>
2024-01-31 17:58:44 -0500

    final commit

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+
+        [HttpGet("Add")]
+        public IActionResult Add(float a, float b)
+        {
+            return Ok(a + b);
+        }
+
+        [HttpGet("Subtract")]
+        public IActionResult Subtract(float a, float b)
+        {
+            return Ok(a - b);
+        }
+
+        [HttpGet("Multiply")]
+        public IActionResult Multiply(float a, float b)
+        {
+            return Ok(a * b);
+        }
+
+        [HttpGet("Divide")]
+        public ActionResult Divide(float a, float b)
+        {
+            if (b == 0)
+            {
+                return BadRequest("Invalid Parameters");
+            }
+            else
+            {
+                return Ok(a / b);
+            }
+        }
+
+        [HttpGet("Modulo")]
+        public ActionResult Modulo(float a, float b)
+        {
+            if (a == 0)
+            {
+                return BadRequest("Invalid Parameters");
+            }
+
+            return Ok(a % b);
+        }
+    }
+}
